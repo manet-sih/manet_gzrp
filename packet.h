32ff4427cf7c9621e66dd4231198b0035377a9b5
prabhdepSP <prabhcoder@gmail.com>
2020-02-22 16:26:13 +0530

    added initial intrazone packet structure

---
+++ packet.h
@@ -0,0 +1,36 @@
+#include "ns3/ipv4-address.h"
+#include "ns3/nstime.h"
+#include "ns3/object-base.h"
+#include "ns3/address-utils.h"
+#include "ns3/packet.h"
+//typeid is not yet discusssed and implemented
+class GzrpPacket: public ns3::Header{
+	private:
+		NS_OBJECT_ENSURE_REGISTERED(GzrpPacket);
+		ns3::Ipv4Address srcIp;
+		uint32_t zoneId;
+		uint32_t seqNum;
+		uint8_t numKnownZones;
+		virtual ns3::TypeId GetInstanceTypeId(void) const;
+		virtual uint32_t GetSerializedSize() const;
+		virtual void Serialize(ns3::Buffer::Iterator start) const;
+		virtual uint32_t Deserialize(ns3::Buffer::Iterator start);
+		virtual void Print(std::ostream &os) const;
+	 
+	public:
+		GzrpPacket();
+		GzrpPacket(ns3::Ipv4Address srcAddr,uint32_t zone,uint32_t seqNo,uint32_t KnownZones);
+		void setSrcIp(ns3::Ipv4Address ip);
+		void setSeqNo(uint32_t seqNo);
+		void setZoneId(uint32_t zId);
+		void setNumKnownZones(uint8_t );
+		ns3::Ipv4Address getSrcIp();
+		uint32_t getSeqNo();
+		uint32_t getZoneId();
+		uint8_t getNumKnownZones();
+};
+
+
+
+	
+
